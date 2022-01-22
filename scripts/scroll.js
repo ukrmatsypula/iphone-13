981953a1e502ed29abbf99eeb318d9b24ff30039
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-22 21:29:19 +0200

    refactor: add encapsulation

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,18 +1,22 @@
-const links = document.querySelectorAll(".header-menu__item a, .card-details__link-characteristics");
+(() => {
+  const links = document.querySelectorAll(
+    ".header-menu__item a, .card-details__link-characteristics"
+  );
 
-seamless.polyfill();
+  seamless.polyfill();
 
-links.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    e.preventDefault();
-    const id = element.getAttribute("href").substring(1);
-    const section = document.getElementById(id);
+  links.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
 
-    if (section) {
-      seamless.elementScrollIntoView(section, {
-        behavior: "smooth",
-        block: "start",
-      });
-    }
+      if (section) {
+        seamless.elementScrollIntoView(section, {
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    });
   });
-});
+})();
