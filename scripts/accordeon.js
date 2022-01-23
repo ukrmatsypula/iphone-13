e8b47289e63237d3088b280db6d9e9e244238b27
Roman Matsypula <ukrmatsypula@gmail.com>
2022-01-24 01:04:46 +0200

    feat: add close item for accordeon

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -5,7 +5,22 @@
     const chButton = item.querySelector(".characteristics__title");
     const chContent = item.querySelector(".characteristics__description");
 
-    chButton.addEventListener("click", function () {
+    chButton.addEventListener("click", () => {
+      // close tab when another is open
+      chItems.forEach(openItem => {
+        if (openItem === item) return;
+
+        const chButtonActive = openItem.querySelector('.characteristics__title');
+        const chContentOpen = openItem.querySelector('.characteristics__description');
+
+        if (chContentOpen.classList.contains('open')) {
+            chContentOpen.style.height = '';
+
+            chButtonActive.classList.remove('active');
+            chContentOpen.classList.remove('open');
+        }
+    });
+
       if (chContent.classList.contains("open")) {
         chContent.style.height = "";
       } else {
